37b6b86c0e4c5eaa6ae32887b2ba944a6ed98e11
Ryan Jackson <ryan@rjackson.ca>
2020-10-29 23:33:55 -0400

    Bashing

--- bash/cron-hour.sh
+++ bash/cron-hour.sh
@@ -4,6 +4,5 @@ DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 
 printf -v TIMESLUG '%(%Y-%m-%d-%H)T' -1
 
-/home/pi/.pyenv/shims/python3 $DIR/../python/ndviMain.py \ 
+/home/pi/.pyenv/shims/python3 $DIR/../python/ndviMain.py \
   && gsutil cp -a public-read $DIR/../data/images/*-$TIMESLUG.png gs://growimages/images
-  
