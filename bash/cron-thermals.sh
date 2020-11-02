427ffee1a984574fd28cb72c466b9f05dae789c8
Ryan Jackson <ryan@rjackson.ca>
2020-11-01 20:05:19 -0500

    Fix sql inserts

--- bash/cron-thermals.sh
+++ bash/cron-thermals.sh
@@ -3,8 +3,6 @@
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 
 # Save Thermals
-echo "Saving data for $TIMESLUG"
-
-bq query \
+bq query --use_legacy_sql=false \
   "INSERT INTO \`grow-293817\`.grow.stats
     (timestamp, tempcpu, temp1, humid1) VALUES (`/home/pi/.pyenv/shims/python3 $DIR/../python/thermals.py`)"
