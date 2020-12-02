e7c50654db6a51034120678a1929fc9e7163f6ef
Ryan Jackson <ryan@rjackson.ca>
2020-12-02 00:30:31 -0500

    wip

--- python/ndviMain.py
+++ python/ndviMain.py
@@ -11,15 +11,18 @@ SCRIPT_PATH = os.path.dirname(os.path.realpath(__file__))
 OUTPUT_PATH = f"{SCRIPT_PATH}/../data/images"
 TIMESTAMP = time.strftime("-%Y-%m-%d-%H")
 
-
+print('Taking Image')
 camera = Camera()
 imageFilename = camera.capture(OUTPUT_PATH)
 
 with Image.open(imageFilename) as image:
+    print('Resizing Image')
     resized_image = image.resize((800,600), Image.LANCZOS)
 
+print('Saving Image')
 resized_image.convert('RGB').save(f"{OUTPUT_PATH}/resized{TIMESTAMP}.jpg", "JPEG")
 
+print('Saving NDVI')
 plt.imsave(f"{OUTPUT_PATH}/ndvi{TIMESTAMP}.jpg",
         ndviTensorFromImage(resized_image), cmap=fastiecm, vmin=-1.0, vmax=1.0)
 
--- python/ndviProcess.py
+++ python/ndviProcess.py
@@ -14,21 +14,3 @@ def ndviTensorFromImage(image):
     redBlueSum[redBlueSum == 0] = 0.01
     arrNDVI = redBlueDiff/redBlueSum
     return arrNDVI
-
-
-
-# def ndvi(image, processedImgFilename, imageOutPath):
-#     img = Image.open(image)
-
-#     imgR, _, imgB, _ = img.split()
-
-#     arrR = numpy.asarray(imgR).astype('float')
-#     arrB = numpy.asarray(imgB).astype('float')
-#     redBlueDiff = (arrR - arrB)
-#     redBlueSum = (arrR + arrB)
-
-#     redBlueSum[redBlueSum == 0] = 0.01
-#     arrNDVI = redBlueDiff/redBlueSum
-
-#     fastiecm = LinearSegmentedColormap.from_list('mylist', colors) 
-#     plt.imsave(imageOutPath, arrNDVI, cmap=fastiecm, vmin=-1.0, vmax=1.0)
