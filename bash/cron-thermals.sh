5fb686fffdab9a66260b10eebc1e202b4eace5b4
Grow Pi <ryan+grow@rjackson.ca>
2021-07-18 23:49:29 +0100

    Updated bash permissions

--- bash/cron-thermals.sh
+++ bash/cron-thermals.sh
@@ -6,13 +6,14 @@ DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 
 bq query --use_legacy_sql=false \
   "INSERT INTO \`grow-293817\`.grow.stats
-    (timestamp, tempcpu, temp1, humid1) VALUES (`/home/pi/.pyenv/shims/python3 $DIR/../python/thermals.py`)"
+   (timestamp, tempcpu, temp1, humid1) VALUES (`/home/pi/.pyenv/shims/python3 $DIR/../python/thermals.py`)"
 
 # Google IOT Core
 
-LABELS = ("cpu_temp_1", "temp_1", "humid_1")
-THERMALS = `/home/pi/.pyenv/shims/python3 ./thermals.py | sed 's/^[^,]*,//' | tr "\," "\n"`
+LABELS=("cpu_temp_1" "temp_1" "humid_1")
+THERMALS=(`/home/pi/.pyenv/shims/python3 $DIR/../python/thermals.py | sed 's/^[^,]*,//' | tr "\," "\n"`)
 
-for i in "${!LABELS[@]}"; do
-    curl -d "name=${LABELS[i]}value=${THERMALS[i]}" http://localhost:5000/track
+for i in "${!THERMALS[@]}"; do
+    curl --data "name=${LABELS[i]}value=${THERMALS[i]}" http://localhost:5000/track
 done
+
