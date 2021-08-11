61a63f4f7e8307b9caf646af62d1fbc2ea1ea18c
Ryan Jackson <ryan@rjackson.ca>
2021-08-11 15:23:16 -0400

    wip

--- ui/app.py
+++ ui/app.py
@@ -27,6 +27,11 @@ def track(cloud=None):
     '''
     if request.method == 'POST':
         print('Received request')
+        app.logger.debug('this is a DEBUG message')
+        app.logger.info('this is an INFO message')
+        app.logger.warning('this is a WARNING message')
+        app.logger.error('this is an ERROR message')
+        app.logger.critical('this is a CRITICAL message')
         try:
             names = [request.form['name']]
             values = [request.form['value']]
@@ -49,12 +54,17 @@ def track(cloud=None):
                 pass
 
         cloud.publish_messages(messages)
+
         message = f"Connection: {enabled}\n" + "\n".join(["{} : {:.2f}"
                     .format(message.get('name', '(not-set)'), message.get("value", '(not-set)')) for message in messages])
-        print(message)
+        app.logger.debug('message')
         return message
 
     return "POST data as name=value= or name[]=value[]="
 
 if __name__ == '__main__':
     app.run()
+else:
+    gunicorn_logger = logging.getLogger('gunicorn.error')
+    app.logger.handlers = gunicorn_logger.handlers
+    app.logger.setLevel(gunicorn_logger.level)
